a46ef4309c07c6845c7031dc60023c6b4680d6ca
nicollyysouza <nickysouza.ribeiro@gmail.com>
2022-10-26 09:22:01 -0300

    Front-bootstrap

--- eventos-app/src/view/login/index.js
+++ eventos-app/src/view/login/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
 
-function Login(){
-    return(
-        <h1>Login</h1>
-    )
+function Login() {
+    return (
+        <div className='login-content d-flex align-items-center'>
+        <main className="form-signin m-auto">
+        <form>
+          <img className="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"></img>
+          <h1 className="h3 mb-3 fw-normal text-center">Login</h1>
+      
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+            <label for="floatingInput">Email </label>
+          </div>
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"></input>
+            <label for="floatingPassword">Senha</label>
+          </div>
+          <button className="w-100 btn btn-lg btn-login" type="submit">Logar</button>
+          
+        </form>
+      </main>
+      </div>
+    );
 }
+
 
 export default Login;
